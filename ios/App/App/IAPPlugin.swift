088c85e7d1e01b22ee4387bf020d20960f4180e4
joshtickner3 <josh.tickner3@gmail.com>
2026-02-24 10:58:35 -0500

    fixes

---
+++ ios/App/App/IAPPlugin.swift
@@ -0,0 +1,8 @@
+//
+//  IAPPlugin.swift
+//  App
+//
+//  Created by Josh Tickner on 2/24/26.
+//
+
+import Foundation
---
+++ ios/App/App/public/IAPPlugin.swift
@@ -0,0 +1,8 @@
+//
+//  IAPPlugin.swift
+//  
+//
+//  Created by Josh Tickner on 2/24/26.
+//
+
+import Foundation
